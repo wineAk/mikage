1285fc08baf3ebc68447b845985eaa9b37ac4382
wineAk <akasaka@interpark.co.jp>
2025-05-19 09:18:10 +0900

    [fix]

--- server/app/routes/api/404.tsx
+++ server/app/routes/api/404.tsx
@@ -1,5 +1,6 @@
 export async function loader() {
   return new Response(JSON.stringify({ data: null, error: "404 Not Found." }), {
+    status: 404,
     headers: { "Content-Type": "application/json" },
   });
 }
