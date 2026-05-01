9a60b0473735963a8283ba85b1ba2bf914954238
wineAk <akasaka@interpark.co.jp>
2026-05-01 13:52:40 +0900

    vite-plugin-devtools-json を削除

--- server/vite.config.ts
+++ server/vite.config.ts
@@ -2,14 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import devtoolsJson from 'vite-plugin-devtools-json';
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), devtoolsJson()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./app"),
